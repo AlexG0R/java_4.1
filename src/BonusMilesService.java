a48be00ac460c0d525218087f836c7391a8d90fc
AlexG0R <klayter@mail.ru>
2021-09-05 14:40:05 +0300

    reformating

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,6 @@
 public class BonusMilesService {
-    public int calculate(double price)
-    {
+    public int calculate(double price) {
         int milePrice = 20;
-        int countMile = (int) price/milePrice;
-        return countMile;
+        return (int) price / milePrice;
     }
 }
